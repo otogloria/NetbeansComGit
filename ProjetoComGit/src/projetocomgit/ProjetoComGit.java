7e2366e38a0abb18685a4ff5f91b086fa71f27c6
Oto Gloria <otogloria@gmail.com>
2017-10-31 07:03:42 -0200

    novo teste

---
+++ ProjetoComGit/src/projetocomgit/ProjetoComGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetocomgit;
+
+/**
+ *
+ * @author Oto Gloria
+ */
+public class ProjetoComGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Olha o Github de novo");
+    }
+    
+}
