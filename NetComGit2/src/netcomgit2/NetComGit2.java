7e05e971981ebe117fd90c0dd9d9de77ce31d204
Oto Gloria <otogloria@gmail.com>
2017-10-31 07:34:41 -0200

    primeiro commit desse projeto

---
+++ NetComGit2/src/netcomgit2/NetComGit2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netcomgit2;
+
+/**
+ *
+ * @author Oto Gloria
+ */
+public class NetComGit2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Enfim, testando o Git");
+    }
+    
+}
